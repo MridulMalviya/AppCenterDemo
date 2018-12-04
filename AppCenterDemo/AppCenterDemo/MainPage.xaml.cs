66e73d18f0fc867b8731adde9ef527eeab13aa2b
Bryan Anthony Garcia <bryan@xam-consulting.com>
2018-12-04 19:47:50 +0800

    Add project files.

---
+++ AppCenterDemo/AppCenterDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCenterDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
